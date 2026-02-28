7fb1e39f9247d14a2c0dc60930cd26bfa68a5235
Leeroy <leeroyvdvliet@outlook.com>
2026-02-28 12:26:50 +0100

    Enable auto migrate on startup

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,14 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
-using PhotoBooth.Domain;
+﻿using PhotoBooth.Data;
+using Microsoft.EntityFrameworkCore;
 
-namespace PhotoBooth.Data;
+var builder = WebApplication.CreateBuilder(args);
 
-public class AppDbContext : DbContext
+// 1️⃣ Pak standaard connection string uit appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// 2️⃣ Check of Render DATABASE_URL gebruikt
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+if (!string.IsNullOrEmpty(databaseUrl))
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options)
-    {
-    }
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':');
 
-    public DbSet<Photo> Photos { get; set; }
+    connectionString =
+        $"Host={uri.Host};" +
+        $"Port={uri.Port};" +
+        $"Database={uri.AbsolutePath.TrimStart('/')};" +
+        $"Username={userInfo[0]};" +
+        $"Password={userInfo[1]};" +
+        $"SSL Mode=Require;Trust Server Certificate=true";
 }
+
+// 3️⃣ Registreer DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
--- Program.cs
+++ Program.cs
@@ -30,4 +30,5 @@ using (var scope = app.Services.CreateScope())
     db.Database.Migrate();
 }
 
+
 app.Run();
