893b295b472dae0880f2a27bd5902ad683fcf14e
Leeroy <leeroyvdvliet@outlook.com>
2026-03-02 11:39:25 +0100

    Remove invalid Services namespace

--- Program.cs
+++ Program.cs
@@ -2,11 +2,9 @@
 using PhotoBooth.Application.Interfaces;
 using PhotoBooth.Application.Services;
 using PhotoBooth.Infrastructure.Data;
-using PhotoBooth.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Alleen standaard connection string gebruiken
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -15,11 +13,10 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddControllers();
+
 builder.Services.AddScoped<IPhotoService, PhotoService>();
 builder.Services.AddScoped<IPhotoRepository, PhotoRepository>();
 builder.Services.AddScoped<PhotoValidationService>();
-
-builder.Services.AddScoped<PhotoBooth.Application.Interfaces.IPhotoRepository, MockPhotoValidator>();
 
 var app = builder.Build();
 
@@ -30,7 +27,6 @@ app.MapControllers();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
-// Auto migrate
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
